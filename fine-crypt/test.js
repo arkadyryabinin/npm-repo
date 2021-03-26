c3e652c01d458a463846f6d0b976f2b4eeef89c8
Arkady Ryabinin <arkadyryabinin@yandex.ru>
2021-03-26 18:33:40 +0300

    testing

--- fine-crypt/test.js
+++ fine-crypt/test.js
@@ -1,12 +1,20 @@
+// test module locally
 const Cryptor = require('./app.js');
 
-const cryptor = new Cryptor('top_secret_word'); // creates a new cryptor object
+// create secret word
+const secret = 'top_secret_word';
 
-// const initText = 'ᔄ';
-const initText = 'Three Men in a Boat (To Say Nothing of the Dog),';
+// create a new cryptor object
+const cryptor = new Cryptor(secret); 
 
-const encryptedText = cryptor.encrypt(initText) // returns encrypted text
-console.log('encryptedText', encryptedText);
-const decryptedText = cryptor.decrypt(encryptedText) // returns 
+// select a text or id for encryption
+// encrypted text should contain only UTF-16 characters
+// with codes between 33 and 1023
+// e.g. latin letters, digits and basic punctuation marks
+const initText = '3 Men in a Boat (To Say Nothing of the Dog),';
 
-console.log(initText, encryptedText, decryptedText);
+// encrypt the text
+const encryptedText = cryptor.encrypt(initText);
+
+// decrypt the text
+const decryptedText = cryptor.decrypt(encryptedText);
